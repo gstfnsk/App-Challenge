659772cdf6dda0333ba27fb01518f8c753a55598
Adriel de Souza <ds.adriel@gmail.com>
2025-06-09 21:32:40 -0300

    change folder structure

---
+++ App-Challenge/Protocols/ViewCodeProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  ViewCodeProtocol.swift
+//  App-Challenge
+//
+//  Created by Adriel de Souza on 09/06/25.
+//
+
+protocol ViewCodeProtocol {
+    func setup()
+    func addSubviews()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+}
+
+
+extension ViewCodeProtocol {
+    func setup() {
+        addSubviews()
+        setupConstraints()
+        setupAdditionalConfiguration()
+    }
+    
+    func setupAdditionalConfiguration(){}
+}
