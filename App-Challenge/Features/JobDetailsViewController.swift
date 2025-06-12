980a8e7bfd7be5ed848c9d618126cd4ea886c672
gstfnsk <giulia.chimini@gmail.com>
2025-06-12 09:09:56 -0300

    remove unecessary alignment, change textview to label, add main stack and add placeholder

--- App-Challenge/Features/JobDetailsViewController.swift
+++ App-Challenge/Features/JobDetailsViewController.swift
@@ -23,8 +23,23 @@ class JobDetailsViewController: UIViewController {
     private lazy var jobDetailsTitle: UILabel = {
             var label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
-            label.text = "Detalhes da Vaga"
-            label.textAlignment = .center
+            label.text = "Recepcionista"
+            label.font = UIFont(name: "SFProRounded-Bold", size: 30)
+            return label
+        }()
+    
+    private lazy var placeholderLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "[placeholder]"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var companyNameTitle: UILabel = {
+            var label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "Le Cochon Volant"
             label.font = UIFont(name: "SFProRounded-Bold", size: 30)
             return label
         }()
@@ -33,32 +48,23 @@ class JobDetailsViewController: UIViewController {
         var label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Descrição da vaga:"
-        label.textAlignment = .left
         return label
     }()
     
-    private lazy var descriptionTextView: UITextView = {
-        var textView = UITextView()
-        textView.isScrollEnabled = false
-            textView.isEditable = false
-            textView.isSelectable = false
-            textView.textContainer.lineBreakMode = .byWordWrapping
-            textView.textContainerInset = .zero
-            textView.textContainer.lineFragmentPadding = 0
-            textView.text = """
-            Estamos em busca de uma recepcionista para integrar nosso time e garantir uma experiência de excelência aos nossos clientes desde o primeiro contato. Se você tem atenção aos detalhes, postura profissional e paixão pela hospitalidade, esta vaga é para você.
-            """
-            textView.textColor = .black
-            textView.backgroundColor = .clear
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.layer.borderWidth = 0
-        textView.tintColor = .systemGray
-//        textView.font
-        return textView
-    }()
+        private lazy var descriptionText: UILabel = {
+            var label = UILabel()
+            label.numberOfLines = 0
+            label.text = """
+                Estamos em busca de uma recepcionista para integrar nosso time e garantir uma experiência de excelência aos nossos clientes desde o primeiro contato. Se você tem atenção aos detalhes, postura profissional e paixão pela hospitalidade, esta vaga é para você.
+                """
+            label.textColor = .black
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = .systemFont(ofSize: 12)
+            return label
+        }()
     
-    lazy var descriptionStack: UIStackView = {
-        var stack = UIStackView(arrangedSubviews: [descriptionLabel, descriptionTextView])
+    private lazy var descriptionStack: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [descriptionLabel, descriptionText])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 8
@@ -69,38 +75,29 @@ class JobDetailsViewController: UIViewController {
         var label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Responsabilidades:"
-        label.textAlignment = .left
         return label
     }()
     
-    private lazy var responsabilitiesTextView: UITextView = {
-        var textView = UITextView()
-        textView.isScrollEnabled = false
-            textView.isEditable = false
-            textView.isSelectable = false
-            textView.textContainer.lineBreakMode = .byWordWrapping
-            textView.textContainerInset = .zero
-            textView.textContainer.lineFragmentPadding = 0
-            textView.text = """
-            - Recepcionar os clientes com cordialidade, elegância e discrição.
-            - Gerenciar reservas (presenciais, por telefone e plataformas digitais).
-            - Organizar a fila de espera e coordenar a ocupação das mesas.
-            - Comunicar-se com a equipe de salão e cozinha para garantir fluidez no atendimento.
-            - Fornecer informações sobre o restaurante, o menu e o serviço, quando necessário.
-            - Resolver imprevistos com empatia e profissionalismo.
-            - Manter postura e apresentação impecáveis, alinhadas ao padrão da casa.
-            """
-            textView.textColor = .black
-            textView.backgroundColor = .clear
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.layer.borderWidth = 0
-        textView.tintColor = .systemGray2
-//        textView.font
-        return textView
+    private lazy var responsabilitiesText: UILabel = {
+        var label = UILabel()
+        label.numberOfLines = 0
+        label.text = """
+                    - Recepcionar os clientes com cordialidade, elegância e discrição.
+                    - Gerenciar reservas (presenciais, por telefone e plataformas digitais).
+                    - Organizar a fila de espera e coordenar a ocupação das mesas.
+                    - Comunicar-se com a equipe de salão e cozinha para garantir fluidez no atendimento.
+                    - Fornecer informações sobre o restaurante, o menu e o serviço, quando necessário.
+                    - Resolver imprevistos com empatia e profissionalismo.
+                    - Manter postura e apresentação impecáveis, alinhadas ao padrão da casa.
+                    """
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12)
+        return label
     }()
     
-    lazy var responsabilitiesStack: UIStackView = {
-        var stack = UIStackView(arrangedSubviews: [responsabilitiesLabel, responsabilitiesTextView])
+    private lazy var responsabilitiesStack: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [responsabilitiesLabel, responsabilitiesText])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 8
@@ -111,39 +108,38 @@ class JobDetailsViewController: UIViewController {
         var label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Requisitos:"
-        label.textAlignment = .left
         return label
     }()
     
-    private lazy var requirementsTextView: UITextView = {
-        var textView = UITextView()
-        textView.isScrollEnabled = false
-            textView.isEditable = false
-            textView.isSelectable = false
-            textView.textContainer.lineBreakMode = .byWordWrapping
-            textView.textContainerInset = .zero
-            textView.textContainer.lineFragmentPadding = 0
-            textView.text = """
+    private lazy var requirementsText: UILabel = {
+        var label = UILabel()
+        label.numberOfLines = 0
+        label.text = """
             - Experiência em atendimento ou recepção em ambientes sofisticados (restaurantes, hotéis ou eventos).
             - Boa comunicação verbal e escrita.
             - Discrição, proatividade e organização.
             - Fluência em português. Desejável conhecimento em francês e/ou inglês.
             - Familiaridade com sistemas de reservas é um diferencial.
             """
-            textView.textColor = .black
-            textView.backgroundColor = .clear
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.layer.borderWidth = 0
-        textView.tintColor = .systemGray2
-//        textView.font
-        return textView
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12)
+        return label
     }()
     
-    lazy var requirementsStack: UIStackView = {
-        var stack = UIStackView(arrangedSubviews: [requirementsLabel, requirementsTextView])
+    private lazy var requirementsStack: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [requirementsLabel, requirementsText])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 8
+        return stack
+    }()
+    
+    lazy var mainStack: UIStackView = {
+        var stack = UIStackView(arrangedSubviews: [placeholderLabel, companyNameTitle, descriptionStack, responsabilitiesStack, requirementsStack])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 13
         return stack
     }()
     
@@ -166,10 +162,9 @@ class JobDetailsViewController: UIViewController {
         return button
     }()
     
-    lazy var buttonsStack: UIStackView = {
+    private lazy var buttonsStack: UIStackView = {
         var stack = UIStackView(arrangedSubviews: [contactButton, shareButton])
         stack.translatesAutoresizingMaskIntoConstraints = false
-//        stack.axis = .vertical
         stack.spacing = 12
         return stack
     }()
@@ -195,15 +190,12 @@ extension JobDetailsViewController: ViewCodeProtocol {
     func addSubviews() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubview(descriptionStack)
-        contentView.addSubview(responsabilitiesStack)
-        contentView.addSubview(requirementsStack)
+        contentView.addSubview(jobDetailsTitle)
+        contentView.addSubview(mainStack)
         contentView.addSubview(buttonsStack)
     }
     
     func setupConstraints() {
-        descriptionLabel.backgroundColor = .red.withAlphaComponent(0.2)
-        descriptionTextView.backgroundColor = .blue.withAlphaComponent(0.2)
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -216,19 +208,15 @@ extension JobDetailsViewController: ViewCodeProtocol {
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            jobDetailsTitle.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            jobDetailsTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            jobDetailsTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            mainStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
+            mainStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            mainStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
 
-            descriptionStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
-            descriptionStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            descriptionStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            responsabilitiesStack.topAnchor.constraint(equalTo: descriptionStack.bottomAnchor, constant: 13),
-            responsabilitiesStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            responsabilitiesStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            requirementsStack.topAnchor.constraint(equalTo: responsabilitiesStack.bottomAnchor, constant: 13),
-            requirementsStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            requirementsStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
             contactButton.widthAnchor.constraint(equalToConstant: 247),
             contactButton.heightAnchor.constraint(equalToConstant: 50),
 
