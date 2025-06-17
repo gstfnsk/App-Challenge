62df11d5bffd171617865502df5897fbc72319ca
AnaPoletto <ana.palhares@edu.pucrs.br>
2025-06-17 15:10:28 -0300

    arrumando sombreado

--- App-Challenge/Components/JobListCell.swift
+++ App-Challenge/Components/JobListCell.swift
@@ -122,9 +122,9 @@ class JobListCell: UICollectionViewCell {
 
     private func setupShadow() {
         layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOpacity = 0.12 // intensidade da sombra (0 a 1)
-        layer.shadowOffset = CGSize(width: 0, height: 4) // direção da sombra
-        layer.shadowRadius = 8 // desfoque da sombra
+        layer.shadowOpacity = 0.15
+        layer.shadowOffset = CGSize(width: 2, height: 2)
+        layer.shadowRadius = 8
         layer.masksToBounds = false
     }
 
