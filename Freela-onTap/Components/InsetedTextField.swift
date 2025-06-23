fa649f499eddd1e2dd326f3ed456551f283ba8cb
Adriel de Souza <ds.adriel@gmail.com>
2025-06-23 17:11:23 -0300

    Add InsetedTextField component for customizable text field insets

---
+++ Freela-onTap/Components/InsetedTextField.swift
@@ -0,0 +1,34 @@
+//
+//  InsetedTextField.swift
+//  Freela-onTap
+//
+//  Created by Adriel de Souza on 23/06/25.
+//
+
+
+
+import UIKit
+
+class InsetedTextField: UITextField {
+    var insetX: Double = 16
+    var insetY: Double = 10
+
+    init(insetX: Double, insetY: Double) {
+        super.init(frame: .zero)
+        
+        self.insetX = insetX
+        self.insetY = insetY
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.insetBy(dx: insetX, dy: insetY)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        textRect(forBounds: bounds)
+    }
+}
